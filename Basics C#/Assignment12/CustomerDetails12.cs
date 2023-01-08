e6c28c117d6821392c9f6de08a8c1d6e0f8953e9
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-01-08 14:23:26 +0530

    Update CustomerDetails12.cs

--- Basics C#/Assignment12/CustomerDetails12.cs
+++ Basics C#/Assignment12/CustomerDetails12.cs
@@ -9,9 +9,6 @@ namespace Assignment12
         public string CustomerName { get; set; }
         public int CustomerAge { get; set; }
         public string CustomerAddress  { get; set; }
-
-        //A shallow copy of an object is a copy whose properties share the same references 
-        //(point to the same underlying values) as those of the source object from which the copy was made.
         
     }
 }
