30fac2fbfa00cb049e7790cde1e1b02515d80ca9
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-01-08 14:24:40 +0530

    Update CustomerDetails12.cs

--- Basics C#/Assignment12/CustomerDetails12.cs
+++ Basics C#/Assignment12/CustomerDetails12.cs
@@ -97,4 +97,141 @@ namespace Assignment12
             throw new Exception("No Account found to delete");
         }
     }
+    class UIStudent
+    {
+        public static CustomerManage msg = null;
+
+        public static void DisplayMenu()
+        {
+            Console.WriteLine("Enter the Size : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            msg = new CustomerManage(size);
+            bool process = true;
+
+            do
+            {
+                Console.WriteLine("==================Customer Management System===================");
+                Console.WriteLine("1...............Add Customer Record");
+                Console.WriteLine("2...............Modify Customer Record");
+                Console.WriteLine("3...............Find Customer Record");
+                Console.WriteLine("4...............Delete Customer Record");
+                Console.WriteLine("5...............Exit");
+                Console.Write("Select option (between 1 to 5) : ");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+                process = ProcessMenu(option);
+
+            } while (process);
+            Console.WriteLine("Thanks for using our application");
+        }
+
+        private static bool ProcessMenu(int option)
+        {
+            switch (option)
+            {
+                case 1:
+                    addingCustomerHelper();
+                    break;
+                case 2:
+                    updatingCustomerHelper();
+                    break;
+                case 3:
+                    findingCustomerHelper();
+                    break;
+                case 4:
+                    deleteCustomerHelper();
+                default:
+                    return false;
+            }
+            return true;
+        }
+
+        private static void addingCustomerHelper()
+        {
+            Console.Write("Enter the Id of Customer : ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Name of Customer : ");
+            string name = Console.ReadLine();
+            Console.Write("Enter the Date Of Age of Customer : ");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Address of Customer : ");
+            string Address = Console.ReadLine();
+
+            Student ctd = new Customer
+            {
+                CustomerID = id,
+                CustomerName = name,
+                CustomerAge = Age,
+                CustomerAddress = Address,
+            };
+            msg.AddCtdDetails(ctd);
+            Console.WriteLine(ctd.CustomerId);
+            Console.Write("Customer Details Added Succesfully");
+
+            //Console.WriteLine("Press Enter to Clear the Screen");
+            //Console.Clear();
+        }
+
+        private static void updatingCustomerHelper()
+        {
+            Console.Write("Enter the Id of Customer : ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Name of Customer : ");
+            string name = Console.ReadLine();
+            Console.Write("Enter the Date Of Age of Customer : ");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Address of Customer : ");
+            string Address = Console.ReadLine();
+
+            Student ctd = new Customer
+            {
+                CustomerID = id,
+                CustomerName = name,
+                CustomerAge = Age,
+                CustomerAddress = Address,
+            };
+
+            msg.UpdateCtdDetails(ctd);
+            Console.WriteLine("Press Enter to Clear the Screen");
+            Console.Clear();
+        }
+
+        private static void findingCustomerHelper()
+        {
+            Console.Write("Enter the Id of Customer : ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                Student ctd = msg.FindCustomer(id);
+                Console.WriteLine("The Details of the Student are as follows:");
+                string content = $"\nThe Student No: {ctd.CustomerId}\nThe Name: {ctd.CustomerName}\nThe Age : {ctd.CustomerAge}\nThe Address : {ctd.CustomerAddress}\n";
+                Console.WriteLine(content);
+                Console.WriteLine("Press Enter to Clear the Screen");
+                Console.Clear();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        public static void deleteCustomerHelper()
+        {
+            Console.Write("Enter the Id of Customer : ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                Student ctd = msg.DeleteCustomer(id);
+                Console.WriteLine("Press Enter to Clear the Screen");
+                Console.Clear();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+    
 }
