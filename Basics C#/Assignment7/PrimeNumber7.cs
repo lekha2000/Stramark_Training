de0aa3abaed74bcf5c3c75017c8b3c91a02b74ea
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-01-11 20:42:42 +0530

    Add files via upload

---
+++ Basics C#/Assignment7/PrimeNumber7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentFunction
+{
+    class PrimeNumber
+    {
+        public static bool Chkprime(int num)
+        {
+            for (int i = 2; i < num; i++)
+                if (num % i == 0)
+                    return false;
+            return true;
+        }
+        public static void Main()
+        {
+            Console.Write("Enter the Number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (Chkprime(n))
+                Console.WriteLine(n + " is a prime number");
+            else
+                Console.WriteLine(n + " is not a prime number");
+        }
+    }
+}
