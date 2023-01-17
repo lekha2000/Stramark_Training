ddb0f15050e67f8fba0acd755eb8d2fab12a6e8c
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-01-17 20:33:55 +0530

    Add files via upload

---
+++ Basics C#/Assignment9/ReverseStr9.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AssignmentFunction
+{
+    class Reverse
+    {
+        public static string reverseStr(string name)
+        {
+            string[] words = name.Split(" ");
+
+            for (int i = words.Length - 1; i > -1; i--)
+            {
+                Console.Write($"{words[i]}" + " ");
+            }
+            return null;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the String");
+            string sentence = Console.ReadLine();
+            reverseStr(sentence);
+        }
+    }
+    
+}
