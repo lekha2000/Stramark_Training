57f79314a7766d8a4beeebfe80b062c3f03aa708
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-01-18 19:36:41 +0530

    Add files via upload

---
+++ Basics C#/Assignment11/SimpleIntrestcs11.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AssignmentFunction
+{
+    class SimpleIntrest
+    {
+        public static float SimpleIntrest(float P, float R, float T)
+        {
+            float SI = (P * R * T) / 100;
+            Console.WriteLine("Simple Intrest :  " + SI);
+            return SI;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Principal Amount : ");
+            float PrincipalAmount = float.Parse(Console.ReadLine());
+            Console.Write("Enter the Intrest Rate : ");
+            float IntrestRate = float.Parse(Console.ReadLine());
+            Console.Write("Enter the Time : ");
+            float Time = float.Parse(Console.ReadLine());
+            SimpleIntrest(PrincipalAmount, IntrestRate, Time);
+
+        }
+    }
+    
+}
