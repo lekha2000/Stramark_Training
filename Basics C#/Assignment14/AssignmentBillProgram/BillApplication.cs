1dcfd4965afd0fe4da091bab55785f2e5bb55117
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-01-25 21:45:22 +0530

    Update BillApplication.cs

--- Basics C#/Assignment14/AssignmentBillProgram/BillApplication.cs
+++ Basics C#/Assignment14/AssignmentBillProgram/BillApplication.cs
@@ -4,3 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+namespace AssignmentBillProgram
+{
+    class Bill
+    {
+        public int BillNo { get; set; }
+        public int BillAmount { get; set; }
+        public string BillholderName { get; set; }
+
+        public void GenrateDate()
+        {
+            DateTime date = DateTime.Now;
+            Console.Write("Dated : ");
+            Console.Write(date.ToShortDateString());
+            Console.WriteLine();
+        }
