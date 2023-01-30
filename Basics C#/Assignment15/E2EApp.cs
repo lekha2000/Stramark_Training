b78aab635846642faf688c8c7d8c5f6bc1901b26
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-01-30 21:17:01 +0530

    Update E2EApp.cs

--- Basics C#/Assignment15/E2EApp.cs
+++ Basics C#/Assignment15/E2EApp.cs
@@ -1,1 +1,7 @@
-
+using SampleDataAccessApp.Practical.Dalayer;
+using SampleDataAccessApp.Practical.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
