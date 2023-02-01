a3f5f5ac002c646590d7161cc71e5fbc23c4702b
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-02-01 21:48:12 +0530

    Update ConnectionDemo.cs

--- Basics C#/Assignment15/ConnectionDemo.cs
+++ Basics C#/Assignment15/ConnectionDemo.cs
@@ -7,3 +7,12 @@ using System.Data.SqlClient;
 using System.Data;
 using Utility;
 
+namespace SampleDataAccessApp
+{
+    static class Database
+    {
+        const string STRCONNECTION = "Data Source=192.168.171.36;Initial Catalog=3316;Integrated Security=True";
+
+        const string STRQUERY = "SELECT * FROM tblEmployee  ";
+        const string STRQRY = "SELECT * FROM Dept ";
+        const string STRQUER = "SELECT * FROM tblEmployee WHERE DeptName = @dname ";
