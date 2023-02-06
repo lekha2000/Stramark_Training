5afe22720d27b420b5df5e9bbbb897d854be4768
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-02-06 18:34:46 +0530

    Update E2EMovieApp.cs

--- Basics C#/Assignment15/E2EMovieApp.cs
+++ Basics C#/Assignment15/E2EMovieApp.cs
@@ -1,1 +1,11 @@
+using SampleDataAccessApp.DataLayerMovie;
+using SampleDataAccessApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
