8a3f1fdf9e08038800d74e0c7dc52242c50f1b95
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-02-06 18:35:05 +0530

    Update E2EMovieApp.cs

--- Basics C#/Assignment15/E2EMovieApp.cs
+++ Basics C#/Assignment15/E2EMovieApp.cs
@@ -9,3 +9,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+namespace SampleDataAccessApp
+{
+    namespace Entity
+    {
+        class Movie
+        {
+            public int MovieId { get; set; }
+            public string MovieTitle { get; set; }
+            public string MovieDuration { get; set; }
+            public int MovieRating { get; set; }
+            public int DirectorId { get; set; }
+        }
+
