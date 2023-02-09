77bb0f9deebc74285b87633f033703b4a0076127
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-02-09 20:36:24 +0530

    Update E2EAppDisease.cs

--- Basics C#/Assignment15/E2EAppDisease.cs
+++ Basics C#/Assignment15/E2EAppDisease.cs
@@ -4,3 +4,15 @@ using System.Data;
 using SampleDataAccessApp.Entities;
 using System.Data.SqlClient;
 
+namespace SampleDataAccessApp
+{
+    namespace Entities
+    {
+        class Disease
+        {
+            public int DiseaseId { get; set; }
+            public string DiseaseName { get; set; }
+            public string DiseaseSeverity { get; set; }
+            public string DiseaseCause { get; set; }
+            public int SymptomId { get; set; }
+        }
