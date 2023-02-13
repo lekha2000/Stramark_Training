cb832e78d3b0725439cd59eb0029abfcdc799061
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-02-13 12:06:04 +0530

    Update Arrays.cs

--- Basics Arrays/Arrays.cs
+++ Basics Arrays/Arrays.cs
@@ -34,3 +34,28 @@ namespace CSharpAssignments
                 Console.WriteLine(dd.AddDays(i));
             }
         }
+        static void Countstr()
+        {
+            Console.WriteLine("Enter The String: ");
+            string str = Console.ReadLine();
+            int alphct = 0;
+            int digtct = 0;
+            //int eqlcnt = 0;
+            int specialcnt = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z')
+                {
+                    alphct++;
+                }
+                else if (str[i] >= 0 && str[i] <= 9)
+                {
+                    digtct++;
+                }
+                else
+                {
+                    specialcnt++;
+                }
+
+                // Console.WriteLine(str[i]);
+            }
