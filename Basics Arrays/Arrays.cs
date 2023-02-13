8220bf50931f750391f58341b70bd0492dc44e40
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-02-13 12:05:38 +0530

    Update Arrays.cs

--- Basics Arrays/Arrays.cs
+++ Basics Arrays/Arrays.cs
@@ -4,3 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+namespace CSharpAssignments
+{
+
+    class Array
+    {
+        static void Trans(int[,] arr)
+        {
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    sum += arr[i, j];
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.Write(sum);
+                Console.WriteLine();
+            }
+        }
