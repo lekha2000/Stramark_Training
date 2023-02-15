dc52cfc71afa4b11e1339e031a1bd19c2e8b787a
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-02-15 08:46:03 +0530

    Update Abstract.cs

--- Basics C#/Assignment13/Abstract.cs
+++ Basics C#/Assignment13/Abstract.cs
@@ -3,3 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+namespace BasicsCsharp
+{
+    enum AccountType { SB, RD , FD };
+    class AccountFactory
+    {
+        public static Account CreditType(AccountType acc)
+        {
+            switch (acc)
+            {
+                case AccountType.SB :
+                    return new SBAccount();
+
+                case AccountType.FD:
+                    return new FDAccount();
+
+                case AccountType.RD:
+                    return new RDAccount();
+                default:
+                    throw new Exception("Please Enter Correct Account Type");
+            }
+        }
+    }
