d60f0969609fa10eedc594e43942c5729f1d30bc
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-02-23 10:23:05 +0530

    Update Product.cs

--- WEB App/ProductWebApp/ProductWebApp/Product.cs
+++ WEB App/ProductWebApp/ProductWebApp/Product.cs
@@ -2,3 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+
+namespace ASPDotNetExample
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductImage { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+    }
