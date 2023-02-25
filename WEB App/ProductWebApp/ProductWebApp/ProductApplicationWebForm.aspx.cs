3c181699477abb042ecdefb39c19ecb875b75810
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-02-25 21:35:17 +0530

    Update ProductApplicationWebForm.aspx.cs

--- WEB App/ProductWebApp/ProductWebApp/ProductApplicationWebForm.aspx.cs
+++ WEB App/ProductWebApp/ProductWebApp/ProductApplicationWebForm.aspx.cs
@@ -6,3 +6,19 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//linked ProductAppDLL to ProductAppWeb
+namespace ProductWebApp
+{
+    public partial class ProductAppWebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                var componet = ProductFactory.GetComponenet();
+                lstProduct.DataSource = componet.GetProducts();
+                lstProduct.DataTextField = "ProductName";
+                lstProduct.DataValueField = "ProductId";
+                lstProduct.DataBind();
+            }
+        }
