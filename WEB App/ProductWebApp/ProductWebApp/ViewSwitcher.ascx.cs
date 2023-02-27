13c29280cb0b926b72b17d2622d9220a1dced502
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-02-27 21:36:23 +0530

    Update ViewSwitcher.ascx.cs

--- WEB App/ProductWebApp/ProductWebApp/ViewSwitcher.ascx.cs
+++ WEB App/ProductWebApp/ProductWebApp/ViewSwitcher.ascx.cs
@@ -7,3 +7,15 @@ using System.Web.Routing;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Microsoft.AspNet.FriendlyUrls.Resolvers;
+namespace ProductWebApp
+{
+    public partial class ViewSwitcher : System.Web.UI.UserControl
+    {
+        protected string CurrentView { get; private set; }
+
+        protected string AlternateView { get; private set; }
+
+        protected string SwitchUrl { get; private set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
