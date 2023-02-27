e3d3afca202c971383f57eb0e523003b4a6fc817
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-02-27 21:36:07 +0530

    Update ViewSwitcher.ascx.cs

--- WEB App/ProductWebApp/ProductWebApp/ViewSwitcher.ascx.cs
+++ WEB App/ProductWebApp/ProductWebApp/ViewSwitcher.ascx.cs
@@ -1,1 +1,9 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.FriendlyUrls.Resolvers;
