918cc0c1a70ed3b04c29a54dc6909ffbe6ae9bf1
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-02-27 21:37:52 +0530

    Update ViewSwitcher.ascx.cs

--- WEB App/ProductWebApp/ProductWebApp/ViewSwitcher.ascx.cs
+++ WEB App/ProductWebApp/ProductWebApp/ViewSwitcher.ascx.cs
@@ -34,3 +34,9 @@ namespace ProductWebApp
                 this.Visible = false;
                 return;
             }
+            var url = GetRouteUrl(switchViewRouteName, new { view = AlternateView, __FriendlyUrls_SwitchViews = true });
+            url += "?ReturnUrl=" + HttpUtility.UrlEncode(Request.RawUrl);
+            SwitchUrl = url;
+        }
+    }
+}
