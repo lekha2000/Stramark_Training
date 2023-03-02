9fca01c5be2f66379289ea8af2bbb5f6006c8938
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-03-02 20:35:30 +0530

    Add files via upload

---
+++ WEB App/ProductWebApp/ProductWebApp/MasterPages/DataCaching.aspx.cs
@@ -0,0 +1,31 @@
+﻿using ProductApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HostitalSoftware.WebForms
+{
+    public partial class DataCaching : System.Web.UI.Page
+    {
+        private void cacheObject()
+        {
+            if (Cache["myData"] == null)
+            {
+                var component = ProductFactory.GetComponenet();
+                var data = component.GetProducts();
+                var textFile = Server.MapPath("MyCache.txt");
+                Cache["myData"] = data;
+                Response.Write("Data Got form the Database");
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            cacheObject();
+            gdview.DataSource = Cache["myData"];
+            DataBind();
+        }
+    }
+}
