58a6e2a0cb5cec86097fe5ba1cd44a6fa571ede6
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-03-03 07:34:59 +0530

    Update LoginPage.aspx.cs

--- WEB App/ProductWebApp/ProductWebApp/MasterPages/LoginPage.aspx.cs
+++ WEB App/ProductWebApp/ProductWebApp/MasterPages/LoginPage.aspx.cs
@@ -6,3 +6,14 @@ using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+namespace ProductWebApp.MasterPages
+{
+    public partial class LoginPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.User.Identity.IsAuthenticated)
+            {
+                lblUSerInfo.Text = "Welcome  " + Page.User.Identity.Name;
+            }
+
