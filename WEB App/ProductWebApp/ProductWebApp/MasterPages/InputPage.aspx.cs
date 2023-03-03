5fc8f9522f69486ba16a9807f9495cbf954d813d
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-03-03 07:26:44 +0530

    Update InputPage.aspx.cs

--- WEB App/ProductWebApp/ProductWebApp/MasterPages/InputPage.aspx.cs
+++ WEB App/ProductWebApp/ProductWebApp/MasterPages/InputPage.aspx.cs
@@ -5,3 +5,12 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+namespace ProductWebApp.MasterPages
+{
+    public partial class InputPage : System.Web.UI.Page
+    {
+        static int count = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            count++;
+        }
