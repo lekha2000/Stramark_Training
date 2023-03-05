aa14bd79b3213cced5a2a432bfe0796f9e98d0e3
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-03-05 13:15:34 +0530

    Update MyFirstCalcApp.aspx.cs

--- WEB App/ProductWebApp/ProductWebApp/MasterPages/MyFirstCalcApp.aspx.cs
+++ WEB App/ProductWebApp/ProductWebApp/MasterPages/MyFirstCalcApp.aspx.cs
@@ -5,3 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+namespace ProductWebApp.MasterPages
+{
+    public partial class MyFirstCalcApp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
