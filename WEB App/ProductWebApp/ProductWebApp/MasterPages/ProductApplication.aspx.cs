ed77dbd5a1a28019f6feba0a87f249dfa0191be6
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-03-06 20:14:05 +0530

    Update ProductApplication.aspx.cs

--- WEB App/ProductWebApp/ProductWebApp/MasterPages/ProductApplication.aspx.cs
+++ WEB App/ProductWebApp/ProductWebApp/MasterPages/ProductApplication.aspx.cs
@@ -12,3 +12,16 @@ namespace ProductWebApp.MasterPages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                var data = ProductRepo.AllProducts;
+                foreach (var item in data)
+                {
+                    lstProduct.Items.Add(new ListItem
+                    {
+                        Text = item.ProductName,
+                        Value = item.ProductId.ToString()
+                    });
+                }
+            }
+        }
