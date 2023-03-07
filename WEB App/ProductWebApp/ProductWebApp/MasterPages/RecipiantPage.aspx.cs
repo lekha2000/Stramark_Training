831867f0f30e0940629babf35eb5f35a8a58e094
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-03-07 07:28:53 +0530

    Update RecipiantPage.aspx.cs

--- WEB App/ProductWebApp/ProductWebApp/MasterPages/RecipiantPage.aspx.cs
+++ WEB App/ProductWebApp/ProductWebApp/MasterPages/RecipiantPage.aspx.cs
@@ -5,3 +5,12 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+namespace ProductWebApp.MasterPages
+{
+    public partial class RecipiantPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            queryStringExample();
+            cookiesExample();
+        }
