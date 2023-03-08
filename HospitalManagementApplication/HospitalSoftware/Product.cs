af2651c4144913fe601b1221cc70f7cbc1af443c
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-03-08 07:47:40 +0530

    Update Product.cs

--- HospitalManagementApplication/HospitalSoftware/Product.cs
+++ HospitalManagementApplication/HospitalSoftware/Product.cs
@@ -4,3 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+namespace ProductApp
+{
+
+    public interface IProduct
+    {
+        void AddProduct(tblProduct product);
+        void UpdateProduct(tblProduct product);
+        void DeleteProduct(int id);
+        List<tblProduct> GetProducts();
+    }
+    
