96074d499fa852cad5fbba59d2f0c5a287cfcd7a
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-03-09 07:45:03 +0530

    Add files via upload

---
+++ HospitalManagementApplication/HospitalSoftware/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using HostitalSoftware.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace HostitalSoftware
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Application["Doctors"] = new DoctorRepo().AllDoctors;
+            Application["Patients"] = new PatientRepo();
+        }
+    }
+}
