443d4e30a02771fb683b894157f165d1c86fc42a
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-03-09 07:45:31 +0530

    Add files via upload

---
+++ HospitalManagementApplication/HospitalSoftware/Helper.cs
@@ -0,0 +1,21 @@
+﻿using HostitalSoftware.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace HostitalSoftware
+{
+    public class Helper
+    {
+        public static void PopulateDoctors(DropDownList dpList, object data)
+        {
+            var doctors = data as List<Doctor>;
+            dpList.DataSource = doctors;
+            dpList.DataTextField = "DoctorName";
+            dpList.DataValueField = "DoctorId";
+            dpList.DataBind();
+        }
+    }
+}
