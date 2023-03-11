617761494f2b94fff5abd66ebebe83b7bda55af0
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-03-11 09:26:34 +0530

    Add files via upload

---
+++ HospitalManagementApplication/HospitalSoftware/Interface/IPatientRepo.cs
@@ -0,0 +1,17 @@
+﻿using HostitalSoftware.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostitalSoftware.Interface
+{
+    interface IPatientRepo
+    {
+        void RegisterNewPatient(Patient patient);
+        void UpdatePatient(Patient patient);
+        List<Patient> GetPatients(int DocId);
+        Patient FindPatient(int PatientId);
+    }
+}
