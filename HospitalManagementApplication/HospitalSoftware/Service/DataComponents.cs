e86a9826a3fa085af02517e3f0184986001c3118
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-03-11 09:30:13 +0530

    Update DataComponents.cs

--- HospitalManagementApplication/HospitalSoftware/Service/DataComponents.cs
+++ HospitalManagementApplication/HospitalSoftware/Service/DataComponents.cs
@@ -4,3 +4,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+namespace HostitalSoftware.Service
+{
+    public class DoctorRepo
+    {
+        public List<Doctor> AllDoctors => new List<Doctor>
+        {
+            new Doctor
+            {
+                DoctorId = 1,
+                DoctorName = "Suresh",
+                Fees = 4500,
+                Specialization = "Heart Surgeon"
+            },
