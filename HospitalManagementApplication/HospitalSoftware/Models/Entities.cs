c85174c54914fc3d625fa37dcf504b799ed35c8b
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-03-12 11:25:44 +0530

    Update Entities.cs

--- HospitalManagementApplication/HospitalSoftware/Models/Entities.cs
+++ HospitalManagementApplication/HospitalSoftware/Models/Entities.cs
@@ -3,3 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+namespace HostitalSoftware.Models
+{
+    public class Doctor
+    {
+        public int DoctorId { get; set; }
+        public string DoctorName { get; set; }
+        public int Fees { get; set; }
+        public string Specialization { get; set; }
+    }
