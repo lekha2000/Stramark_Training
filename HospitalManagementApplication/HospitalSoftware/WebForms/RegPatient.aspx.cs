54e57726652354eb411fed3486b75ada31e7cbd5
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-03-13 07:18:35 +0530

    Update RegPatient.aspx.cs

--- HospitalManagementApplication/HospitalSoftware/WebForms/RegPatient.aspx.cs
+++ HospitalManagementApplication/HospitalSoftware/WebForms/RegPatient.aspx.cs
@@ -1,1 +1,10 @@
+using HostitalSoftware.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HostitalSoftware.Models;
+
 
