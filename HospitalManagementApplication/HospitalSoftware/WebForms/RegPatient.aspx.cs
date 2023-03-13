89e550719b638cc334ed1eb9e6cc478ad7e596ce
Lekha Raju <62109221+lekha2000@users.noreply.github.com>
2023-03-13 07:18:54 +0530

    Update RegPatient.aspx.cs

--- HospitalManagementApplication/HospitalSoftware/WebForms/RegPatient.aspx.cs
+++ HospitalManagementApplication/HospitalSoftware/WebForms/RegPatient.aspx.cs
@@ -8,3 +8,16 @@ using System.Web.UI.WebControls;
 using HostitalSoftware.Models;
 
 
+namespace HostitalSoftware.WebForms
+{
+    public partial class RegPatient : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Helper.PopulateDoctors(dpDoctors, Application["Doctors"]);
+            }
+
+        }
+
